7f9fac8deb265ab6c23d4f80062d3d4115b1a371
Felipe Bermudez <anfbermudezme@unal.edu.co>
2025-06-12 20:47:38 +0200

    feat: add COURSE_PUBLISHED signal in lms settings.

--- platform_plugin_aspects/apps.py
+++ platform_plugin_aspects/apps.py
@@ -55,6 +55,12 @@ class PlatformPluginAspectsConfig(AppConfig):
                 PluginSignals.RECEIVERS: [
                     {
                         # The name of the app's signal receiver function.
+                        PluginSignals.RECEIVER_FUNC_NAME: "receive_course_publish",
+                        # The full path to the module where the signal is defined.
+                        PluginSignals.SIGNAL_PATH: "xmodule.modulestore.django.COURSE_PUBLISHED",
+                    },
+                    {
+                        # The name of the app's signal receiver function.
                         PluginSignals.RECEIVER_FUNC_NAME: "receive_course_enrollment_changed",
                         # The full path to the module where the signal is defined.
                         PluginSignals.SIGNAL_PATH: "common.djangoapps.student.signals.signals.ENROLL_STATUS_CHANGE",
